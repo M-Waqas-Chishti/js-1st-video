a11928b77d1bdce25cb0a85d8661dd08bd6b4ca9
M-Waqas-Chishti <waqaschishtiinfo@gmail.com>
2026-06-09 08:43:02 +0000

    Conversion of datatypes

---
+++ ConversionOperation.js
@@ -0,0 +1,31 @@
+//let score = "33ab"
+//let score = null
+// let score = true
+let score = false
+
+//let score = undefined
+console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33ab" =>NaN
+// true => 1 ; false +> 0;
+let isLoggedIN = 1
+//let isLoggedIN = ""
+//let isLoggedIN = "waqas"
+
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+
+// 1 =>trur ; 0 => false 
+//"" => false
+// "waqas " => true
+
+let somenumber = 33
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);

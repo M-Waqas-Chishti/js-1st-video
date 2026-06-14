c92a5961099fb08e7a9dcdd58ec7fd78267c54f9
M-Waqas-Chishti <waqaschishtiinfo@gmail.com>
2026-06-14 10:00:39 +0000

    Primitive & Reference

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,14 @@
+// Primitive
+// 7 types : String, Number, Bollearn, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue  = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+ 
+
+//Reference (Non Primitive)
+// Array, Objects, Functions, 

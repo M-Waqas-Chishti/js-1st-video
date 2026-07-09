d3e52c3d5d2133495d2f97e22b8da51aa6fe0f8d
M-Waqas-Chishti <waqaschishtiinfo@gmail.com>
2026-07-09 21:02:44 +0500

    Add files via upload

---
+++ 02LetConst.js
@@ -0,0 +1,22 @@
+ console.log("javaScript tutorial 3");
+ const author = "M.Waqas Chishti"
+ // author = 5 // Throws an error becouse constant cannot be changed 
+ var a = 45;
+ var c = null;
+ var d = undefined; 
+ /* var b = "chishti"
+ {
+    var b = "waqas"
+    console.log(b);
+    
+ }
+ console.log(b); */
+  let  b = "chishti"
+ {
+    let b = "waqas"
+    console.log(b);
+    
+ }
+ console.log(b);
+ 
+ 

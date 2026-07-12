7a36a7502d778ce0ffa5f6dfddf56f6f72fa14e9
Muhammad Waqas Chishti <waqaschishtiinfo@gmail.com>
2026-07-12 10:55:18 +0500

    Add files via upload

---
+++ 03_primitives_objectives.js
@@ -0,0 +1,22 @@
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345
+let c = true; // can also be false
+let d = BigInt("567") + BigInt(3)
+let e = "waqas"
+let f = Symbol("I am a nice symbol")
+let g = undefined
+console.log(a, b , c, d , e, f ,g);
+console.log(typeof d);
+
+// Non Primitive Data Type -  Objects in Js
+const item = {
+    "Harry": true,
+    "waqas": false,
+    "Ilyas": 67,
+    "Ali": undefined
+}
+console.log(item["waqas"]);
+
+
+

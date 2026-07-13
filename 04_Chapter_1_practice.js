8178015640aed392cd1dcce92f4893846d392f9f
Muhammad Waqas Chishti <waqaschishtiinfo@gmail.com>
2026-07-13 22:47:13 +0500

    Add files via upload

---
+++ 04_Chapter_1_practice.js
@@ -0,0 +1,33 @@
+// Chaper 1 Q1
+let a = "Waqas"
+let b = 6
+console.log(a + b);
+
+// Chapter 1 - Q2
+
+console.log(typeof (a + b) );
+
+// Chapter 1 - Q3
+
+const c = {
+    Name: "Waqas",
+    Section : 1,
+    isPrincipal: false
+}
+
+// Chapter 1 - Q4
+
+c['friend'] = "Ali"
+c['name'] = "Irfan"
+console.log(c);
+
+// Chapter 1 - Q5
+
+const dict = {
+    appreciate: "recongnize the full worth of.",
+    ataraxia:"a state of freedom trom emotional disturbance and anxiety",
+    yakka: "work, especially hard work."
+}
+// console.log(dict.yakka);
+console.log(dict['yakka']);
+

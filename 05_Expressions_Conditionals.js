a293703d3cfc540e1ebe904d35b37aabb97b029f
Muhammad Waqas Chishti <waqaschishtiinfo@gmail.com>
2026-07-15 21:05:06 +0500

    Add files via upload

---
+++ 05_Expressions_Conditionals.js
@@ -0,0 +1,35 @@
+console.log("Operators in Js");
+// Arithmetic Operators
+let a = 45;
+let b = 4;
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a / b = ",a/b);
+console.log("a * b = ",a*b);
+console.log("a ** b = ",a**b);
+console.log("a % b = ",a%b);
+console.log("a++  = ",a++);
+console.log("a-- = ",a--);
+
+// Assignment Operators 
+let c = 5;
+ c += 5; // same as c = c + 5
+ console.log(c);
+ 
+ // Comparison Operators 
+
+ let comp1 = 6;
+ let comp2 = 5;
+ console.log("comp1 == comp2 is ", comp1 == comp2);
+ console.log("comp1 != comp2 is ", comp1 != comp2);
+ console.log("comp1 === comp2 is ", comp1 === comp2);
+ console.log("comp1 !== comp2 is ", comp1 !== comp2);
+ console.log("comp1 > comp2 is ", comp1 > comp2);
+
+  // Logical Operators 
+
+  let x = 5;
+  let y = 6;
+  console.log(x < y && x == 5);
+  console.log(x > y && x == 5);
+  

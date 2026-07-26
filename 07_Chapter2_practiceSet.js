83dc7ca1c2936e8b6945b0282b139b6915603a35
Muhammad Waqas Chishti <waqaschishtiinfo@gmail.com>
2026-07-26 18:08:49 +0500

    Add files via upload

---
+++ 07_Chapter2_practiceSet.js
@@ -0,0 +1,49 @@
+// Question 1
+
+let a = prompt("Enter a age")
+if(a > 10 && a < 20){
+ alert("Yea lies between 10 and 20")
+}
+
+// Question 2
+
+let b = prompt ("What is your age?")
+switch(b){
+    case '12':
+        console.log("Your age is 12");
+        break;
+    case '18':
+        console.log("Your age is 18");
+        break;
+    case '14':
+        console.log("Your age is 14");
+        break;
+              
+    default:
+        console.log("Your age is not special ");
+        
+}
+
+// Question 3
+
+let num = prompt("Enter a number:")
+num = Number(num);
+if (num % 2 == 0 && num % 3 == 0){
+    console.log("Yes number is divisible by 2 and 3 "); 
+}
+else{
+    console.log("Number is not divisible by 2 and 3");
+    
+}
+
+// Question 4
+
+let num = prompt("Enter a number:")
+num = Number(num);
+if (num % 2 == 0 && num % 3 == 0){
+    console.log("Yes number is divisible by 2 and 3 "); 
+}
+else{
+    console.log("Number is not divisible by 2 and 3");
+    
+}

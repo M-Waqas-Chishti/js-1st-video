300ad0c0eeabeb9437239c9455a683d91c9f8891
Muhammad Waqas Chishti <waqaschishtiinfo@gmail.com>
2026-07-31 12:46:28 +0500

    Add files via upload

---
+++ 10_While_loop.js
@@ -0,0 +1,19 @@
+let n = prompt("Wnter the value if n")
+n = Number(n)
+let i = 0 
+while(i<n)
+{
+    console.log(i);
+    i++
+}
+
+// Do_While loop
+
+let n = prompt("Wnter the value if n")
+n = Number(n)
+let i = 10
+do
+{
+    console.log(i);
+    i++
+}while(i<n)
